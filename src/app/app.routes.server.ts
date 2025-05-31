582148a60a81d6aea8a232996dfbe380f078cc46
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-31 18:47:42 -0300

    fix: resolve issue with prerender not receiving params for :lang/home route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':lang/home',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { lang: 'pt-br' },
+        { lang: 'en-us' }
+      ];
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
