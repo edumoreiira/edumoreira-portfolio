0208fae98ee79186a48884127e804306d556e4cb
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-09-10 21:35:16 -0300

    feat(project-card): design layout

---
+++ src/app/components/shared/project-card/project-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../base/button.component';
+
+@Component({
+  selector: 'article[app-project-card]',
+  host: { 
+    class: 'flex flex-col gap-3 p-5 rounded-xl'
+  },
+  imports: [ButtonComponent],
+  templateUrl: './project-card.component.html',
+})
+export class ProjectCardComponent {
+
+}
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ProjectCardComponent } from "../../components/shared/project-card/project-card.component";
+import { GlowingBorderDirective, GlowingBorderItemDirective } from "../../directives/glowing-border.directive";
 
 @Component( {
   selector: 'page-projects',
-  imports: [],
+  imports: [ProjectCardComponent, GlowingBorderDirective, GlowingBorderItemDirective],
   templateUrl: './projects.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectsComponent {
+
+  projects = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ];
 
 }
