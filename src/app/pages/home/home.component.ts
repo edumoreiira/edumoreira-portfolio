e538c57c945660ffa2a9fa1315a017ef23b3cb3a
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-09-20 21:13:41 -0300

    feat(home): define button toggle texts and animations

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,73 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { LANGUAGE_APPLICATION } from '../../tokens/language.tokens';
 import { ButtonComponent } from '../../components/base/button.component';
 import { createAnimation } from '../../animations/default-transitions.animations';
 import { IntersectionObserverDirective } from '../../directives/intersection-observer.directive';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { EmButtonToggleGroupComponent } from '../../components/shared/button-toggle/em-button-toggle-group.component';
+import { EmButtonToggleDirective } from '../../components/shared/button-toggle/em-button-toggle.directive';
+import { EmButtonToggleAnimationDirective } from '../../components/shared/button-toggle/em-button-toggle-animation.directive';
+import { HomeView, UserPreferenceService } from '../../services/user-preference.service';
+import { FormsModule } from '@angular/forms';
+import { filter, startWith } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, IntersectionObserverDirective, RouterOutlet],
+  imports: [IntersectionObserverDirective, RouterOutlet, EmButtonToggleGroupComponent,
+    EmButtonToggleDirective, EmButtonToggleAnimationDirective, FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   animations: [
-    createAnimation('fadeInUp', { transform: 'translateY(3rem)', duration: '700ms' })
+    createAnimation('fadeInUp', { transform: 'translateY(3rem)', duration: '700ms' }),
+    createAnimation('textAnimate', { animateY: true,  transform: 'translateY(2rem)', duration: '500ms' }),
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
   protected lg = inject(LANGUAGE_APPLICATION);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly userPreference = inject(UserPreferenceService)
+  // 
+  homePageSelector = signal<HomeView | ''>('');
+
+  changeRoute = effect(() => {
+    const route = this.homePageSelector();
+    if(route) {
+      this.router.navigate([route], { relativeTo: this.route })
+      this.userPreference.setHomeView(route);
+    }
+  })
+
+  constructor() {
+    // this code will update signal homePageSelector when routes changes between /developer - /freelancer
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      startWith(null), //starts with null, forcing subscribe when page loads for the first time.
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(() => {
+      const childRoute = this.route.firstChild;
+      if (childRoute) {
+        const currentView = childRoute.snapshot.url[0]?.path as HomeView;
+        // prevents infinite loop with effect
+        if(currentView && this.homePageSelector() !== currentView) {
+          this.homePageSelector.set(currentView);
+        }
+      }
+    });
+  }
+
+
+
+
+
+  
   // Methods
+
+
   scrollTo(id: string) {
     const element = document.getElementById(id);
     if (element) {
