3111da69f1aad75d57cae9af44678b410a74f2d1
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-09-25 15:28:53 -0300

    chore(document-listener.service): add support to viewport signals

--- src/app/services/document-listener.service.ts
+++ src/app/services/document-listener.service.ts
@@ -1,64 +1,78 @@
-import { Injectable, Renderer2, RendererFactory2, Signal, signal } from "@angular/core";
+import {
+  computed,
+  Injectable,
+  Renderer2,
+  RendererFactory2,
+  Signal,
+  signal,
+} from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class DocumentListenerService {
-    private renderer: Renderer2;
-    private eventSignal = signal<MouseEvent | KeyboardEvent | null>(null);
-    private screenSize = signal(0);
-    private scrollFromTop = signal(0);
-    // 
-    screenSize$ = this.screenSize.asReadonly();
-    get event$(): Signal<MouseEvent | KeyboardEvent | null> {
-        return this.eventSignal.asReadonly();
-    }
-    scrollFromTop$ = this.scrollFromTop.asReadonly();
-    
-    constructor(rendererFactory: RendererFactory2) {
-        this.renderer = rendererFactory.createRenderer(null, null);
-        this.registerClickEventListener();
-        this.registerKeyboardEventListener();
-        this.registerScreenResizeListener();
-        this.registerScrollEventListener();
-        this.updateScreenSize();
-    }
-    
-    private registerClickEventListener() {
-        this.renderer.listen('document', 'click', (event: MouseEvent) => {
-            this.eventSignal.set(event);
-        })
-    }
+  private renderer: Renderer2;
+  private eventSignal = signal<MouseEvent | KeyboardEvent | null>(null);
+  private screenSize = signal(0);
+  private scrollFromTop = signal(0);
+  //
+  screenSize$ = this.screenSize.asReadonly();
+  get event$(): Signal<MouseEvent | KeyboardEvent | null> {
+    return this.eventSignal.asReadonly();
+  }
+  scrollFromTop$ = this.scrollFromTop.asReadonly();
+  //viewports
+  readonly isXs = computed(() => this.screenSize() < 640);
+  readonly isSm = computed(() => this.screenSize() >= 640);
+  readonly isMd = computed(() => this.screenSize() >= 768);
+  readonly isLg = computed(() => this.screenSize() >= 1024);
+  readonly isXl = computed(() => this.screenSize() >= 1280);
+  readonly is2xl = computed(() => this.screenSize() >= 1536);
 
-    private registerKeyboardEventListener() {
-        this.renderer.listen('document', 'keydown', (event: KeyboardEvent) => {
-            this.eventSignal.set(event);
-        })
-    }
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+    this.registerClickEventListener();
+    this.registerKeyboardEventListener();
+    this.registerScreenResizeListener();
+    this.registerScrollEventListener();
+    this.updateScreenSize();
+  }
+  
+  private registerClickEventListener() {
+    this.renderer.listen('document', 'click', (event: MouseEvent) => {
+      this.eventSignal.set(event);
+    });
+  }
 
-    private registerScreenResizeListener() {
-        this.renderer.listen('window', 'resize', () => {
-            this.updateScreenSize();
-        })
-    }
+  private registerKeyboardEventListener() {
+    this.renderer.listen('document', 'keydown', (event: KeyboardEvent) => {
+      this.eventSignal.set(event);
+    });
+  }
 
-    private registerScrollEventListener() {
-        this.renderer.listen('window', 'scroll', () => {
-            this.updateScrollPositionFromTop();
-        })
-    }
+  private registerScreenResizeListener() {
+    this.renderer.listen('window', 'resize', () => {
+      this.updateScreenSize();
+    });
+  }
 
-    private updateScreenSize() {
-        if(typeof window !== 'undefined') {
-            this.screenSize.set(window.innerWidth);
-        }
-    }
+  private registerScrollEventListener() {
+    this.renderer.listen('window', 'scroll', () => {
+      this.updateScrollPositionFromTop();
+    });
+  }
 
-    private updateScrollPositionFromTop() {
-        if (typeof window !== 'undefined') {
-            const distanceFromTop = window.scrollY || document.documentElement.scrollTop;
-            this.scrollFromTop.set(distanceFromTop);
-        }
+  private updateScreenSize() {
+    if (typeof window !== 'undefined') {
+      this.screenSize.set(window.innerWidth);
     }
+  }
 
+  private updateScrollPositionFromTop() {
+    if (typeof window !== 'undefined') {
+      const distanceFromTop =
+        window.scrollY || document.documentElement.scrollTop;
+      this.scrollFromTop.set(distanceFromTop);
+    }
+  }
 }
